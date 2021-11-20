0272bb8a5a90494768c06fd7d542ae4971c0c209
alfrin <alfrin.saldanha@outlook.com>
2021-11-20 15:28:20 -0600

    Tweaked cors to include cors origin

--- config/passport.js
+++ config/passport.js
@@ -39,15 +39,31 @@ const strategy  = new LocalStrategy(customFields, verifyCallback);
 
 passport.use(strategy);
 
+
+// BELOW WORKS -- commented for testing
+// passport.serializeUser((user, done) => {
+//     console.log("Seralized user: ", user)
+//     done(null, user.id);
+// });
+
+// passport.deserializeUser((userId, done) => {
+//     User.findById(userId)
+//         .then((user) => {
+//             done(null, user);
+//         })
+//         .catch(err => done(err))
+// });
+
+//TESTING BELOW::::11/20
 passport.serializeUser((user, done) => {
     console.log("Seralized user: ", user)
-    done(null, user.id);
+    return done(null, user.id);
 });
 
 passport.deserializeUser((userId, done) => {
     User.findById(userId)
         .then((user) => {
-            done(null, user);
+            return done(null, user);
         })
         .catch(err => done(err))
 });
